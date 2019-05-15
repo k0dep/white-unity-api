7721ce1831f890b85e6976642716bcb4c1123850
Stanislav Petrov <sv.petrov@kodep.pro>
2019-05-15 23:22:04 +0300

    extend packag info

--- DataAccess/Migrations/20190515201728_ExtendedPackageModel.Designer.cs
+++ DataAccess/Migrations/20190515201728_ExtendedPackageModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhiteUnity.DataAccess.Context;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(PackagesDbContext))]
-    partial class PackagesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190515201728_ExtendedPackageModel")]
+    partial class ExtendedPackageModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DataAccess/Models/PackageModel.cs
+++ DataAccess/Models/PackageModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace WhiteUnity.DataAccess.Models
@@ -24,7 +25,23 @@ namespace WhiteUnity.DataAccess.Models
         [Required]
         public string UrlForManifest { get; set; }
 
+        [MaxLength(300)]
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Version { get; set; }
+
+        public IList<PackageDependencyModel> Dependencies { get; set; }
+
+        public string Category { get; set; }
+
+        public string Unity { get; set; }
+
         public DateTime AddedTimestamp { get; set; }
+
         
         public int LooksCount { get; set; }
     }
